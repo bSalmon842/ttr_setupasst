07912a4a1f2bb43b001878178d4f5eeff1ee3a79
Brock Salmon <bg100197@gmail.com>
2019-11-08 01:30:39 +1000

    Added reverted build.bat changes

--- code/build.bat
+++ code/build.bat
@@ -1,10 +1,10 @@
 @echo off
 
 REM -MTd for debug build
-set commonFlagsCompiler=-MTd -nologo -Gm- -GR- -fp:fast -EHa -Od -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -wd4505 -wd4130 -FC -Z7
+set commonFlagsCompiler=-MTd -nologo -Gm- -GR- -fp:fast -EHa -Od -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -wd4505 -wd4130 -wd4996 -FC -Z7
 set commonFlagsLinker= -incremental:no -opt:ref -subsystem:console
 
 IF NOT EXIST ..\build mkdir ..\build
 pushd ..\build
-cl %commonFlagsCompiler% ..\code\ttr_setupasst.cpp /link -incremental:no -opt:ref
+cl %commonFlagsCompiler% ..\code\ttr_setupasst.cpp /link -incremental:no -opt:ref wininet.lib
 popd
